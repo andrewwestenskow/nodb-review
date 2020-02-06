5382193900c9a0eb5b7671a03b903f16a82ba181
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-02-06 11:36:32 -0700

    Build out component tree

---
+++ src/components/Pokedex.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Pokemon from './Pokemon'
+
+function Pokedex(props) {
+  return (
+    <div>
+      Pokedex.js
+      <Pokemon />
+    </div>
+  )
+}
+
+export default Pokedex
---
+++ src/components/Pokemon.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Pokemon extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isEditing: false,
+      userInput: '',
+    }
+  }
+
+  toggleEdit() {}
+
+  handleChange(e) {}
+
+  render() {
+    return <div>Pokemon</div>
+  }
+}
+
+export default Pokemon
