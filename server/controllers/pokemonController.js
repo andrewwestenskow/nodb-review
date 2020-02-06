1d8eafd69981a98aec6e8765635da48594297dbc
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-02-06 12:17:24 -0700

    Built back end file structure

---
+++ server/controllers/pokemonController.js
@@ -0,0 +1,13 @@
+const caughtPokemon = []
+
+let id = 0
+
+module.exports = {
+  getCaughtPokemon: (req, res) => {},
+
+  catchPokemon: (req, res) => {},
+
+  editPokemonName: (req, res) => {},
+
+  releasePokemon: (req, res) => {},
+}
